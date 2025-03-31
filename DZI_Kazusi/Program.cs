c596a652e0221693a4d30eb9c50a818f3ba8c4b9
ipstoyanov20 <ipstoyanov20@codingburgas.bg>
2025-03-31 10:26:09 +0300

    initial

---
+++ DZI_Kazusi/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DZI_Kazusi;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tema1 tema1 = new Tema1();
+        tema1.Run();
+    }
+}
---
+++ DZI_Kazusi/Tema17.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+public class Tema1
+{
+    public void Run()
+    {
+        int age = int.Parse(Console.ReadLine());
+        float washingMachinePrice = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        int presentPrice = int.Parse(Console.ReadLine());
+        int savings = 0;
+        int moneyFromPresents = 0;
+        int bonus = 0;
+        for (int i = 1; i <= age; i++) {
+            if (i % 2 == 0)
+            {
+                bonus += 10;
+                savings += bonus;
+                savings -= 1;
+            }
+            else {
+                moneyFromPresents += presentPrice;
+            }
+        }
+        var allMoney = savings + moneyFromPresents;
+        if (allMoney >= washingMachinePrice) 
+            Console.WriteLine("Yes! {0}", (allMoney - washingMachinePrice).ToString("F2",CultureInfo.InvariantCulture));
+        else
+            Console.WriteLine("No! {0}", (washingMachinePrice - allMoney).ToString("F2",CultureInfo.InvariantCulture));
+    
+    }
+    
+}
